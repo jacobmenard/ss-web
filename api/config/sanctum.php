8ccf15dced0c3c87d77d3914fcf695a7b7f2d654
Menard Catayas <jemenard082713@gmail.com>
2025-03-22 02:10:20 +0800

    adjustment

--- api/config/sanctum.php
+++ api/config/sanctum.php
@@ -16,7 +16,7 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s', 'sipsandsparks-web.netlify.app', 
+        '%s%s', 'sipsandsparks-web.netlify.app/login', 
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
