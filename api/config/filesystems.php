f29b3453d70e6c89caf43b34f0b9c716bd4d5cd9
Menard Catayas <jemenard082713@gmail.com>
2025-04-08 22:22:55 +0800

    filesystem adjustment

--- api/config/filesystems.php
+++ api/config/filesystems.php
@@ -34,6 +34,17 @@ return [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             // 'throw' => false,
+            
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0700,
+                ],
+            ],
         ],
 
         'public' => [
