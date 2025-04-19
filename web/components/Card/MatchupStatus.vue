582088b9e227059f0070e32457b9e19207db2581
Menard Catayas <123menard@noreply.com>
2025-04-20 01:56:43 +0800

    adjustment

--- web/components/Card/MatchupStatus.vue
+++ web/components/Card/MatchupStatus.vue
@@ -7,7 +7,7 @@
 <template>
     <div class="matchup-status-container">
         <img v-if="props.status == 2" src="~assets/images/friend.svg" alt="">
-        <img v-if="props.status == 3" src="~assets/images/date.svg" alt="">
-        <img v-if="!props.status || props.status == 1" src="~assets/images/none.svg" alt="">
+        <img v-else-if="props.status == 3" src="~assets/images/date.svg" alt="">
+        <img v-else src="~assets/images/none.svg" alt="">
     </div>
 </template>
