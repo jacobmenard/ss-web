f8a2ac87dfb33a7a7bdecb8d5aeb532cd9d2387b
Menard Catayas <123menard@noreply.com>
2025-04-21 03:17:44 +0800

    ad

--- api/app/Http/Controllers/UserEventController.php
+++ api/app/Http/Controllers/UserEventController.php
@@ -334,23 +334,17 @@ class UserEventController extends Controller
                 $item->matchup_user_to_owner_notes = $matchupUser->matchup_notes;
             } else {
                 
-                $item->matchup_user_to_owner = null;
+                $item->matchup_user_to_owner = '1';
                 $item->matchup_user_to_owner_notes = null;
             }
 
-            $item->matchup_final = $item->matchup_status;
-            
-            // if ($item->matchup_status) {
-            //     if ($item->matchup_status == 3 && $matchupUser->matchup_status == 3) {
-            //         $item->matchup_final = 3;
-            //     } else if ($item->matchup_status == 1 || $matchupUser->matchup_status == 1) {
-            //         $item->matchup_final = 1;
-            //     } else {
-            //         $item->matchup_final = 2;
-            //     }
-            // } else {
-            //     $item->matchup_final = 1;
-            // }
+            if ($item->matchup_status == 3 && $matchupUser->matchup_user_to_owner == 3) {
+                $item->matchup_final = 3;
+            } else if ($item->matchup_status == 1 || $matchupUser->matchup_user_to_owner == 1) {
+                $item->matchup_final = 1;
+            } else {
+                $item->matchup_final = 2;
+            }
 
             $item->matchup_owner->profile_image = ENV('AWS_S3_BUCKET_URI') . $item->matchup_owner->profile_image;
             $item->matchup_user->profile_image = ENV('AWS_S3_BUCKET_URI') . $item->matchup_user->profile_image;
