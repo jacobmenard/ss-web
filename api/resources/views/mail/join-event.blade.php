df5c3086aebd20a3c532e585dba125cd4c53f490
Menard Catayas <123menard@noreply.com>
2025-06-15 07:24:24 +0800

    adjustment

---
+++ api/resources/views/mail/join-event.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <p>Hello!</p>
+    <p>We’re so excited to have you join us for today’s event!</p>
+    <p>Important Info About Our New Match System:</p>
+    <p>We’ve upgraded to our own custom-built matching software to make your experience even better!</p>
+    <br>
+    <p>To access the match system:</p>
+    <p>Go to <a href="https://sipsandsparks.com">sipsandsparks.com</a> and click “Match Form”</p>
+    <p>OR go directly to <a href="https://sipsandsparks.com/login">sipsandsparks.com/login</a></p>
+    <p>Use the email you registered with on Eventbrite</p>
+    <p>Use the temporary password <strong>123456</strong></p>
+    <p>Or click “Forgot Password” to get a reset link and create your own password.</p>
+    <p>Once you’re logged in</p>
+    <p>Click the top right corner to upload a profile picture and fill out a short bio and some fun facts. This helps others get to know you better.</p>
+    <br>
+    <p>Important</p>
+    <p>You won’t be able to view today’s event or the participant list until you’ve been checked in. Once we officially begin, you’ll be able to make your selections for each person (date, friend, or none) and leave private notes to help jog your memory when your results are sent out tomorrow.</p>
+    <p>Please arrive 15 to 30 minutes early to check in, get settled, and be ready to meet some great people. The host will begin right at the scheduled time, and we may not be able to accommodate late arrivals.</p>
+    <br>
+    <p>
+        We can’t wait to see you!
+        <br>
+        With love,
+    </p>
+
+</div>
